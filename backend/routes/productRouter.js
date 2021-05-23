3252774b184fa74354f7aa8fea3e2606e827ef6d
Andrew Zang <andrewzang@Andrews-MacBook-Pro.local>
2021-05-23 19:36:15 -0400

    backend update

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,29 +1,23 @@
 import express from 'express';
-import Product from '../models/productModel.js';
-import AsyncHandler from 'express-async-handler';
-
 const router = express.Router();
+import {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview,
+  getTopProducts,
+} from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.get(
-  '/',
-  AsyncHandler(async (req, res) => {
-    const products = await Product.find({});
-
-    res.json(products);
-  })
-);
-
-router.get(
-  '/:id',
-  AsyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-  })
-);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
+router.get('/top', getTopProducts);
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
